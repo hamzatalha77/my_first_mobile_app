af3500f99b9044db959a78ec20d1595aad84eb67
hamzatalha77 <hamza_ta7@hotmail.com>
2024-05-13 13:55:36 +0100

    commit

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
-import { Button } from 'react-native-web'
+import { Button, StyleSheet, Text, View } from 'react-native'
 
 export default function App() {
   return (
