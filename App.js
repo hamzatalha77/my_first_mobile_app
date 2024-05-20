c4136a00fbcaccddf5e904af35d120e156f92a15
hamzatalha77 <hamza_ta7@hotmail.com>
2024-05-20 19:04:21 +0100

    commit

--- App.js
+++ App.js
@@ -6,9 +6,6 @@ export default function App() {
 
 const styles = StyleSheet.create({
   appContainer: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
+    padding: 50
   }
 })
