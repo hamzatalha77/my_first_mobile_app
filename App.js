f20e406550808dbdf0ac9a3d423eb515dd71cc5a
hamzatalha77 <hamza_ta7@hotmail.com>
2024-05-21 12:00:06 +0100

    commit

--- App.js
+++ App.js
@@ -1,7 +1,14 @@
 import { Button, StyleSheet, Text, View } from 'react-native'
+import { TextInput } from 'react-native-web'
 
 export default function App() {
-  return <View style={styles.appContainer}></View>
+  return (
+    <View style={styles.appContainer}>
+      <View>
+        <TextInput></TextInput>
+      </View>
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
