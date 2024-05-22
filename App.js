22f9fef69c746ef3c518f8ae5ef9b0ded0841647
hamzatalha77 <hamza_ta7@hotmail.com>
2024-05-22 14:35:46 +0100

    commit

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <View style={styles.appContainer}>
       <View>
-        <TextInput placeholder="type something" />
+        <TextInput placeholder="type something in" />
         <Button title="add something" />
       </View>
     </View>
