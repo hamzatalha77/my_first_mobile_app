ef94b1635068f12d1a37207a8a623ae0226b9c9e
hamzatalha77 <hamza_ta7@hotmail.com>
2024-05-24 10:36:55 +0100

    commit

--- App.js
+++ App.js
@@ -8,7 +8,7 @@ export default function App() {
         <TextInput placeholder="type something in this section" />
         <Button title="add something" />
       </View>
-      <Text>List </Text>
+      <Text>List of </Text>
     </View>
   )
 }
